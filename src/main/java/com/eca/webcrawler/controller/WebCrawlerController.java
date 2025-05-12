c004507d48c63f41d1fef0cee2ed77a92d4057a0
priyankabhoyar999 <priyankabhoyar999@gmail.com>
2025-05-13 01:12:43 +0200

    removed unused import

--- src/main/java/com/eca/webcrawler/controller/WebCrawlerController.java
+++ src/main/java/com/eca/webcrawler/controller/WebCrawlerController.java
@@ -3,7 +3,6 @@ package com.eca.webcrawler.controller;
 import com.eca.webcrawler.model.WebCrawlerResult;
 import com.eca.webcrawler.service.WebCrawlerService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
